e4be68d942a393c41882cfefd98b108455ee4cfb
Henry Varona <henry.varona@gmail.com>
2017-02-01 22:43:40 -0400

    - Added BitcoinAddres - Calculate The address in bitcoinAccount

--- src/main/java/de/bitsharesmunich/cryptocoincore/base/CryptoCoinManager.java
+++ src/main/java/de/bitsharesmunich/cryptocoincore/base/CryptoCoinManager.java
@@ -8,6 +8,8 @@ public abstract class CryptoCoinManager<T extends CryptoCoinAccount> {
     
     public abstract T newAccount();
     
+    public abstract T getAccount(CryptoCoinAccountSeed seed);
+    
     public abstract T getAccountFromJsonSeed(String jsonSeed);
     
 }
